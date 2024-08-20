9d82f2c1b57521223d83a8afb339e66fdd15eb2c
crimme <kueppers.fabian@web.de>
2024-08-20 10:05:37 +0200

    add contact sorted

--- js/add_contact.js
+++ js/add_contact.js
@@ -1,31 +1,29 @@
 function openAddContact() {
   let addContact = document.getElementById("addContact");
   let overlay = document.getElementById("overlay");
-    
+
   addContact.style.right = "50%"; // Popup nach links schieben
   overlay.style.display = "flex"; // Overlay sichtbar machen
   overlay.addEventListener("click", closeContactPopupByOverlay); // Event-Listener hinzufügen
-  document.getElementById('details-mobile-add-btn').style.background = `var(--darkLightBlue)`; // ändert Farbe vom Hinzufügen Button
+  document.getElementById(
+    "details-mobile-add-btn"
+  ).style.background = `var(--darkLightBlue)`; // ändert Farbe vom Hinzufügen Button
 }
-
 
 function closeContactPopup() {
   let addContact = document.getElementById("addContact");
   let overlay = document.getElementById("overlay");
-  
+
   addContact.style.right = "-1200px"; // Popup nach rechts außerhalb des Bildschirms verschieben
   overlay.style.display = "none"; // Overlay unsichtbar machen
   overlay.removeEventListener("click", closeContactPopupByOverlay); // Event-Listener entfernen
-  document.getElementById('details-mobile-add-btn').style.background = `var(--darkGray)`; // ändert Farbe vom Hinzufügen Button zurück
-
+  document.getElementById(
+    "details-mobile-add-btn"
+  ).style.background = `var(--darkGray)`; // ändert Farbe vom Hinzufügen Button zurück
 }
-
 
 function closeContactPopupByOverlay(event) {
   if (event.target.id === "overlay") {
-      closeContactPopup();
+    closeContactPopup();
   }
 }
-
-
-
