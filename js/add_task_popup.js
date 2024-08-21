985dfada6fe4badfb206bd834485fdc8cc22b888
crimme <kueppers.fabian@web.de>
2024-08-21 09:08:01 +0200

    validatetasks

--- js/add_task_popup.js
+++ js/add_task_popup.js
@@ -1,25 +1,35 @@
 function openAddTaskPopup() {
-    let addTaskPopup = document.getElementById("addTaskPopup");
-    let overlayAddTaskPopup = document.getElementById("overlayAddTaskPopup");
-      
-    addTaskPopup.style.right = "50%"; // Popup nach links schieben
-    overlayAddTaskPopup.style.display = "flex"; // Overlay sichtbar machen
-    overlayAddTaskPopup.addEventListener("click", closeAddTaskPopupByOverlay); // Event-Listener hinzufügen
+  let addTaskPopup = document.getElementById("addTaskPopup");
+  let overlayAddTaskPopup = document.getElementById("overlayAddTaskPopup");
+
+  addTaskPopup.style.right = "50%"; // Popup nach links schieben
+  overlayAddTaskPopup.style.display = "flex"; // Overlay sichtbar machen
+  overlayAddTaskPopup.addEventListener("click", closeAddTaskPopupByOverlay); // Event-Listener hinzufügen
+}
+
+function closeAddTaskPopup() {
+  let addTaskPopup = document.getElementById("addTaskPopup");
+  let overlayAddTaskPopup = document.getElementById("overlayAddTaskPopup");
+
+  addTaskPopup.style.right = "-1200px"; // Popup nach rechts außerhalb des Bildschirms verschieben
+  overlayAddTaskPopup.style.display = "none"; // Overlay unsichtbar machen
+  overlayAddTaskPopup.removeEventListener("click", closeAddTaskPopupByOverlay); // Event-Listener entfernen
+}
+
+function closeAddTaskPopupByOverlay(event) {
+  if (event.target.id === "overlayAddTaskPopup") {
+    closeAddTaskPopup();
   }
-  
-  
-  function closeAddTaskPopup() {
-    let addTaskPopup = document.getElementById("addTaskPopup");
-    let overlayAddTaskPopup = document.getElementById("overlayAddTaskPopup");
-    
-    addTaskPopup.style.right = "-1200px"; // Popup nach rechts außerhalb des Bildschirms verschieben
-    overlayAddTaskPopup.style.display = "none"; // Overlay unsichtbar machen
-    overlayAddTaskPopup.removeEventListener("click", closeAddTaskPopupByOverlay); // Event-Listener entfernen
+}
+
+function validateTaskTitle() {
+  let x = document.getElementById("addTaskInputTitle").value;
+  let xName = document.getElementById("addTitleError");
+  if (x == "") {
+    xName.innerHTML = "Please enter a Title";
+    return false;
+  } else {
+    xName.innerHTML = "";
+    /* return validateEmail(); */
   }
-  
-  
-  function closeAddTaskPopupByOverlay(event) {
-    if (event.target.id === "overlayAddTaskPopup") {
-        closeAddTaskPopup();
-    }
-  }
+}
