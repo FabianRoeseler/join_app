9da5b88359647b42d9c96096f76d933787c346df
crimme <kueppers.fabian@web.de>
2024-08-27 00:02:37 +0200

    added more JSDOCS

--- js/edit_contact.js
+++ js/edit_contact.js
@@ -1,25 +1,26 @@
+/**
+ * Handling for the contact edit popup
+ */
 function openEditContact() {
-    let editContact = document.getElementById("editContact");
-    let overlayEdit = document.getElementById("overlayEdit");
-      
-    editContact.style.right = "50%"; // Popup nach links schieben
-    overlayEdit.style.display = "flex"; // Overlay sichtbar machen
-    overlayEdit.addEventListener("click", closeEditContactPopupByOverlay); // Event-Listener hinzufügen
+  let editContact = document.getElementById("editContact");
+  let overlayEdit = document.getElementById("overlayEdit");
+
+  editContact.style.right = "50%"; // Popup nach links schieben
+  overlayEdit.style.display = "flex"; // Overlay sichtbar machen
+  overlayEdit.addEventListener("click", closeEditContactPopupByOverlay); // Event-Listener hinzufügen
+}
+
+function closeEditContactPopup() {
+  let editContact = document.getElementById("editContact");
+  let overlayEdit = document.getElementById("overlayEdit");
+
+  editContact.style.right = "-1200px"; // Popup nach rechts außerhalb des Bildschirms verschieben
+  overlayEdit.style.display = "none"; // Overlay unsichtbar machen
+  overlayEdit.removeEventListener("click", closeEditContactPopupByOverlay); // Event-Listener entfernen
+}
+
+function closeEditContactPopupByOverlay(event) {
+  if (event.target.id === "overlay") {
+    closeEditContactPopup();
   }
-  
-  
-  function closeEditContactPopup() {
-    let editContact = document.getElementById("editContact");
-    let overlayEdit = document.getElementById("overlayEdit");
-    
-    editContact.style.right = "-1200px"; // Popup nach rechts außerhalb des Bildschirms verschieben
-    overlayEdit.style.display = "none"; // Overlay unsichtbar machen
-    overlayEdit.removeEventListener("click", closeEditContactPopupByOverlay); // Event-Listener entfernen
-  }
-  
-  
-  function closeEditContactPopupByOverlay(event) {
-    if (event.target.id === "overlay") {
-        closeEditContactPopup();
-    }
-  }
+}
